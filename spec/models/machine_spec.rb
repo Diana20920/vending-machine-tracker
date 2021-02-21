45699bab5c7ed94674dfc03cc198090c75c22595
Diana Buffone <diana.pamelaet@gmail.com>
2021-02-20 22:58:54 -0500

    Pass model test relationshipps

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Machine, type: :model do
+  describe 'relationships' do
+    it { should have_many :snack_machines }
+    it { should have_many(:snacks).through(:snack_machines) }
+  end
+
   describe 'validations' do
     it { should validate_presence_of :location }
     it { should belong_to :owner }
